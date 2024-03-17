0d70a20f4b482da031b191cae8fa83f9a1859ea7
Alex Diaz <alex@akdev.xyz>
2024-03-16 21:17:49 -0400

    chore: add root aws.akdev.xyz zone

--- lib/dns-root-stack.ts
+++ lib/dns-root-stack.ts
@@ -1,9 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_iam as iam, aws_route53 as route53 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as akdev from './akdev-constructs';
 
 export class DnsRootStack extends cdk.Stack {
   readonly githubActionsOidcProvider: akdev.GithubActionsOidcProvider;
+  readonly awsRootZone: route53.PublicHostedZone;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -13,5 +15,23 @@ export class DnsRootStack extends cdk.Stack {
       repo: 'aws-dns-root',
       branch: 'main',
     });
+
+    this.awsRootZone = new route53.PublicHostedZone(this, 'RootDnsZone', {
+      zoneName: 'aws.akdev.xyz',
+    });
+
+    const delegates = [
+        new iam.AccountPrincipal('749640853560'),
+    ];
+    delegates.forEach(delegate => this.awsRootZone.grantDelegation(delegate));
+
+    const output: Record<string, string> = {
+      RootZoneId: this.awsRootZone.hostedZoneId,
+      RootNameservers: cdk.Fn.join(', ', this.awsRootZone.hostedZoneNameServers ?? ["none"]),
+    };
+    Object.keys(output)
+      .forEach(key => new cdk.CfnOutput(this, `${key}Output`, {
+        value: output[key],
+      }));
   }
 }
